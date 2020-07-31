6f79131c50236dce4b2daa5d79c1274ef455be4e
hase07180 <yuukisoccer07180@gmail.com>
2020-07-31 15:36:34 +0900

    アプリケーションscssの修正&出品ページのビューの修正

---
+++ app/assets/ javascripts/new.js
@@ -0,0 +1,7 @@
+// $(function() {
+//   $(".fileUp-area__image").on("click", function(){
+//     let file = $(`.js-file`).last();
+//     // console.log(file)
+//     file.trigger("click");
+//   })
+// })
