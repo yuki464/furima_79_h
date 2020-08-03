413df79be371880d5765374282d905deb4e280a7
yuki464 <orenomusuko555@gmail.com>
2020-08-03 14:19:08 +0900

    ルートいじった

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,8 @@ Rails.application.routes.draw do
       get 'category/get_category_grandchildren', to: 'items#get_category_grandchildren', defaults: { format: 'json' }
     end
 end
-resources :credit_cards, only: [:new, :show, :destroy] do
-  collection do
+resources :credit_cards, only: [:index,:new, :show, :destroy] do
+  member do
     post 'pay', to: 'credit_cards#pay'
   end
 end
