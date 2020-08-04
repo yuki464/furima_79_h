6af04a9b4f6ceb01c9ca549f77719e1831390e93
morizo04 <omorikazuki99@gmail.com>
2020-08-04 17:02:59 +0900

    ユーザーのビューを作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :basic_auth, if: :production?
 
   private
@@ -11,5 +12,10 @@ class ApplicationController < ActionController::Base
   end
   def production?
     Rails.env.production?
+  end
+
+  private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
   end
 end
