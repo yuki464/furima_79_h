2cb7516666d3f97d643de5a08f68397f27c6ae59
yuki464 <orenomusuko555@gmail.com>
2020-08-04 13:56:03 +0900

    クレジットカードのコンフリクト対策

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,22 @@ Rails.application.routes.draw do
   root 'items#index'
   resources :items do
     collection do
+      get "buy", to: "items#buy"
       get 'category/get_category_children', to: 'items#get_category_children', defaults: { format: 'json' }
       get 'category/get_category_grandchildren', to: 'items#get_category_grandchildren', defaults: { format: 'json' }
     end
-end
+    # 本来はメンバーでやりたのだが現状はidがないのでコレクションでビューを確認
+    # member do
+    #   get "buy", to: "items#buy"
+    # end
+resources :credit_card, only: [:create, :show, :edit] do
+  collection do
+    post 'delete', to: 'credit_card#delete'
+    post 'show'
+  end
+  member do
+    get 'confirmation'
+     end
+    end 
+  end
 end
