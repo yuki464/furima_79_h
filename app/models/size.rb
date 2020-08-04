d5698632b846efd8834343f4da033cc1d0403bbb
hase07180 <yuukisoccer07180@gmail.com>
2020-08-04 15:26:48 +0900

    active_hash
    active_hashを用いたモデルに
    データを挿入しました。

--- app/models/size.rb
+++ app/models/size.rb
@@ -1,2 +1,14 @@
 class Size < ActiveHash::Base
+  self.data = [
+    {id: 1, name: "XXS以下"},
+    {id: 2, name: "XS(SS)"},
+    {id: 3, name: "S"},
+    {id: 4, name: "M"},
+    {id: 5, name: "L"},
+    {id: 6, name: "XL"},
+    {id: 7, name: "2XL"},
+    {id: 8, name: "3XL"},
+    {id: 9, name: "4XL以上"},
+    {id: 10, name: "FREE SIZE"}
+  ]
 end
