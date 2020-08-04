155a7b7ef52b0ed3340650e2409e8171b4d50177
yuki464 <orenomusuko555@gmail.com>
2020-08-04 18:12:37 +0900

    マージ回避よう
    購入機能のコントローラに更新

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,6 @@
 class ItemsController < ApplicationController
+  require 'payjp'
+  before_action :set_card
   def index
   end
   def new
@@ -19,4 +21,38 @@ class ItemsController < ApplicationController
   
     @category_grandchildren = Category.find(params[:child_id]).children
   end
+  def buy
+    if @card.blank?
+      redirect_to new_card_path
+    else
+      Payjp.api_key = Rails.application.credentials[:PAYJP_PRIVATE_KEY]
+      #保管した顧客IDでpayjpから情報取得
+      customer = Payjp::Customer.retrieve(@card.customer_id) 
+      #カード情報表示のためインスタンス変数に代入
+      @default_card_information = customer.cards.retrieve(@card.card_id)
+    end
+  end
+
+  def pay
+    Payjp.api_key = Rails.application.credentials[:PAYJP_PRIVATE_KEY]
+    Payjp::Charge.create(
+      :amount => @item.price, #支払金額を引っ張ってくる
+      :customer => @card.customer_id,  #顧客ID
+      :currency => 'jpy',              #日本円
+    )
+    redirect_to done_item_buyers_path #完了画面に移動
+  end
+
+  private
+
+  def set_card
+    @card = Card.find_by(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+
+
 end
