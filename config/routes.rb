200fec2af5c064d3b83222a6a0f3d7babe688894
yuki464 <orenomusuko555@gmail.com>
2020-08-05 11:02:09 +0900

    購入完了画面作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   root 'items#index'
   resources :items do
     collection do
+      get "done",to:"items#done"
+      post "pay", to: "items#pay"
       get "buy", to: "items#buy"
       get 'category/get_category_children', to: 'items#get_category_children', defaults: { format: 'json' }
       get 'category/get_category_grandchildren', to: 'items#get_category_grandchildren', defaults: { format: 'json' }
