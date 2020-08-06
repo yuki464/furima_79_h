ef1e6d2fbd6fba5282d74fd39a31e43a27a0f4e4
hase07180 <yuukisoccer07180@gmail.com>
2020-08-06 20:24:09 +0900

    コントローラーの変更
    ①createメソッドの編集
    ②ストロングパラメーターのキーを
    brand　➡︎　brand_idに変更

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,7 +3,7 @@ class ItemsController < ApplicationController
   end
   def new
     @item = Item.new
-    # @item.item_images.new
+    @item.item_images.build
 
     #カテゴリボックスの定義
     @category_parent_array = Category.where(ancestry: nil)
@@ -20,10 +20,16 @@ class ItemsController < ApplicationController
 
   def create
      @item = Item.new(item_params)
+     if @item.save
+      redirect_to root_path
+     else
+      @item.item_images.build
+      render :new
+     end
   end
 
   private
   def item_params
-    params.require(:item).permit(:item_image, :name, :introduction, :price, :brand, :condition_id, :postage_payer, :prefecture_id, :size_id, :preparationday_id, :postagetype_id)
+    params.require(:item).permit(:item_image, :name, :introduction, :price, :brand_id, :condition_id, :postage_payer, :prefecture_id, :size_id, :preparationday_id, :postagetype_id)
   end
 end
