3aa3dce1de90cedee44b06d9b4cf179efeb625f5
hase07180 <yuukisoccer07180@gmail.com>
2020-08-07 15:47:25 +0900

    画像投稿のJS

--- app/models/item.rb
+++ app/models/item.rb
@@ -13,6 +13,7 @@ class Item < ApplicationRecord
   validates :price, presence: true
 
   has_many :item_images, dependent: :destroy
+  accepts_nested_attributes_for :item_images, allow_destroy: true
   belongs_to :category
   belongs_to :brand
 
