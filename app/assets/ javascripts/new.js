bc2aa1c29d7251767d020726879d5362b38f7055
hase07180 <yuukisoccer07180@gmail.com>
2020-08-07 17:46:31 +0900

    画像のサイズ調整と画像の削除機能追加

--- app/assets/ javascripts/new.js
+++ app/assets/ javascripts/new.js
@@ -10,10 +10,28 @@ $(function(){
     //読み込みが完了すると、srcにfileのURLを格納
     fileReader.onloadend = function() {
       var src = fileReader.result
-      var html= `<img src="${src}" width="114" height="80">`
+      var html= `<div class='item-image' data-image="${file.name}">
+                    <div class=' item-image__content'>
+                        <div class='item-image__content--icon'>
+                          <img src=${src} width="114" height="80" class="images">
+                        </div>
+                    </div>
+                    <div class='item-image__operetion'>
+                      <div class='item-image__operetion--delete'>削除</div>
+                    </div>
+                  </div>`
       //image_box__container要素の前にhtmlを差し込む
       $('#image-box__container').before(html);
     }
     fileReader.readAsDataURL(file);
   });
+
+  $(document).on("click", '.item-image__operetion--delete', function(){
+    //プレビュー要素を取得
+    var target_image = $(this).parent().parent()
+    //プレビューを削除
+    target_image.remove();
+    //inputタグに入ったファイルを削除
+    file_field.val("")
+  })
 });
