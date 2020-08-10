54296226f97191074cf90d19be5cd4b8e400d370
morizo04 <omorikazuki99@gmail.com>
2020-08-10 17:32:58 +0900

    セキュリティの強化
    ビューだけでなくコントローラでも削除する前に確認するようにしました

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -38,7 +38,7 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    if @item.destroy
+    if @item.user_id==current_user.id && @item.destroy
       redirect_to root_path, notice: '削除しました'
     else
       flash.now[:alert] = '削除できませんでした'
