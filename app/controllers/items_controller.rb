e25f13e129c9dc8521e4b49e9a74c028a1b17257
yuki464 <orenomusuko555@gmail.com>
2020-08-10 13:33:42 +0900

    アイテムへ移動

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   require "payjp"
-  before_action :set_card, only:[:show]
-  before_action :set_item, only:[:destroy,:show,:edit,:update]
+  before_action :set_card, only:[:buy,:pay]
+  before_action :set_item, only:[:destroy,:show,:edit,:update,:buy,:pay]
   def index
     @items = Item.all
   end
@@ -36,13 +36,50 @@ class ItemsController < ApplicationController
     @category_grandchild = Category.find(@category_id)
   end
   def destroy
-    @item= ItemImage.find(params[:id])
+    @item= ItemImage.where(params[:id])
     if @item.delete
       redirect_to root_path, notice: '削除しました'
     else
       flash.now[:alert] = '削除できませんでした'
       render :show
     end
+  end
+
+  def buy
+    @card = Card.where(user_id: current_user.id).first
+    # #Cardテーブルは前回記事で作成、テーブルからpayjpの顧客IDを検索
+    if @card.blank?
+      #登録された情報がない場合にカード登録画面に移動
+      redirect_to controller: "card", action: "new"
+    else
+      Payjp.api_key = Rails.application.credentials[:payjp][:payjp_access_key]
+      #保管した顧客IDでpayjpから情報取得
+      @customer = Payjp::Customer.retrieve(@card.customer_id)
+      #保管したカードIDでpayjpから情報取得、カード情報表示のためインスタンス変数に代入
+      @default_card_information = @customer.cards.retrieve(@card.card_id)
+    end
+  end
+
+  def pay
+    @card = Card.where(user_id: current_user.id).first
+    Payjp.api_key = Rails.application.credentials[:payjp][:payjp_access_key]
+    Payjp::Charge.create(
+    amount: @item.price, 
+    customer: @card.customer_id, #顧客ID
+    currency: 'jpy', #日本円
+  )
+
+  @item_buyer= Item.find(params[:id])
+      if @item_buyer.update( buyer_id: current_user.id)
+      redirect_to action:  done
+       else
+      flash.now[:alert] = '正しく商品を購入できませんでした。'
+      render :buy
+      end
+  end 
+
+  def done
+    # @user = User.find(current_user.id)
   end
 
   def create
